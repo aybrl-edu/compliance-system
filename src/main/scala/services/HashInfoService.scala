2e0acc4e2d206956929d4955987fdd272e933518
Aybrl <contact@aybrl.com>
2022-12-04 18:20:06 +0100

    impl hash service

--- src/main/scala/services/HashInfoService.scala
+++ src/main/scala/services/HashInfoService.scala
@@ -1,20 +1,31 @@
 package services
 
 import org.apache.spark.sql.DataFrame
-import io.jvm.uuid._
-import java.util.UUID
+import org.apache.spark.sql.functions.{col, when}
+
 
 
 class HashInfoService extends IService {
   override def execute(df : DataFrame, uid  : Long): DataFrame = {
 
-    def uuid = java.util.UUID.randomUUID.toString
+    /*def uuid = java.util.UUID.randomUUID.toString
 
     df2 = df("Firstname",df.Firstname.uuid)
     df3 = df.withColumn("Lastname",df.Lastname.randomUUID().toString)
     df4 = df.withColumn("Address",df.Address.randomUUID().toString)
 
-      //randomUUID().toString
+      //randomUUID().toString*/
+    val rand = new scala.util.Random
+    val UUID = s"${uid}-${rand}"
+
+    val updateDF = df.withColumn("idClient",
+      when(col("idClient") === uid, uid)
+        .when(col("FirstName"), UUID)
+        .when(col("LastName"), UUID)
+        .when(col("Address"), UUID)
+      .otherwise(UUID))
+
+    updateDF
   }
 
 }
