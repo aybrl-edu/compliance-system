b0586b209fae5a0b3bc3ab7b414d913a146066a1
JULIEN CANNOUX <julien.cannoux@etu.u-pec.fr>
2022-12-04 02:10:54 +0100

    V1 for hash part for the 2nd step of service

--- src/main/scala/services/HashInfoService.scala
+++ src/main/scala/services/HashInfoService.scala
@@ -1,11 +1,20 @@
 package services
 
 import org.apache.spark.sql.DataFrame
+import io.jvm.uuid._
+import java.util.UUID
+
 
 class HashInfoService extends IService {
   override def execute(df : DataFrame, uid  : Long): DataFrame = {
-    null
-  }
 
+    def uuid = java.util.UUID.randomUUID.toString
+
+    df2 = df("Firstname",df.Firstname.uuid)
+    df3 = df.withColumn("Lastname",df.Lastname.randomUUID().toString)
+    df4 = df.withColumn("Address",df.Address.randomUUID().toString)
+
+      //randomUUID().toString
+  }
 
 }
