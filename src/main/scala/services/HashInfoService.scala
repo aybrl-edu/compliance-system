9cfa80e167d45295043c1091f36951bf7a035e10
Aybrl <contact@aybrl.com>
2022-12-05 14:00:45 +0100

    hashing with withColumn fun

--- src/main/scala/services/HashInfoService.scala
+++ src/main/scala/services/HashInfoService.scala
@@ -1,7 +1,7 @@
 package services
 
 import models.UserDataInfo
-import org.apache.spark.sql.{Dataset, SparkSession}
+import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.functions.{col, when}
 
 class HashInfoService extends IService {
@@ -11,17 +11,19 @@ class HashInfoService extends IService {
 
     import workers.HDFSFileManager.sparkSession.implicits._
 
-    val updateDS = ds.filter(row => row.idClient == uid)
+    // Testing new approach
+    val update = ds.filter(user => user.idClient == uid)
       .map(user => {
         user.firstName -> UUID
         user.lastName -> UUID
         user.address -> UUID
       }).as[UserDataInfo]
 
-    /*val updateDS : Dataset[UserDataInfo] = ds.withColumn("idClient", when(col("idClient") === uid, uid))
+    val updateDS : Dataset[UserDataInfo] = ds.withColumn("idClient", when(col("idClient") === uid, uid))
       .withColumn("FirstName", when(col("idClient") === uid, UUID).otherwise(col("FirstName")))
       .withColumn("LastName", when(col("idClient") === uid, UUID).otherwise(col("LastName")))
-      .withColumn("Address", when(col("idClient") === uid, UUID).otherwise(col("Address")))*/
+      .withColumn("Address", when(col("idClient") === uid, UUID).otherwise(col("Address")))
+      .as[UserDataInfo]
 
     updateDS
   }
