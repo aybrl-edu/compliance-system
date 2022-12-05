3736627cb260c4324042cc3bae1e042447811322
Aybrl <contact@aybrl.com>
2022-12-05 10:41:13 +0100

    fixing hashing idClient

--- src/main/scala/services/HashInfoService.scala
+++ src/main/scala/services/HashInfoService.scala
@@ -10,7 +10,7 @@ class HashInfoService extends IService {
     val rand = new scala.util.Random
     val UUID : String = s"${uid}-${rand.nextInt()}"
 
-    val updateDF = df.withColumn("idClient", when(col("idClient") === uid, uid))
+    val updateDF = df.withColumn("idClient", when(col("idClient") === uid, uid).otherwise(col("idClient")))
       .withColumn("FirstName", when(col("idClient") === uid, UUID).otherwise(col("FirstName")))
       .withColumn("LastName", when(col("idClient") === uid, UUID).otherwise(col("LastName")))
       .withColumn("Address", when(col("idClient") === uid, UUID).otherwise(col("Address")))
